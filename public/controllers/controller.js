270bd7f0d34ce933882f0edc0f1c5d1bdb0e7f03
Dan Bilde <mihaibilde@gmail.com>
2015-05-05 21:59:21 +0200

    Added the CRUD buttons

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,9 +3,26 @@ var myApp = angular.module('myApp' , []);
 myApp.controller('AppCtrl',['$scope','$http', function($scope,$http){
     console.log("Hello world from controller");
 
+var refresh = function(){
 $http.get('/contactlist').success(function(response){
     console.log("I got the data i requested");
     $scope.contactlist = response;
-});
+    $scope.contact = "";
+    });
+};
+
+refresh();
+
+$scope.addContact = function() {
+    console.log($scope.contact);
+    $http.post('/contactlist' , $scope.contact).success(function(response){
+        console.log(response);
+        refresh();
+    });
+};
+
+$scope.removeContact = function(id){
+    console.log(id);
+};
 
 }]);
--- routes/index.js
+++ routes/index.js
@@ -2,70 +2,81 @@ var express = require('express');
 var router = express.Router();
 var appdata = require('../data.json');
 var app = express();
-var mongojs = require('mongojs');
-var db = mongojs('contactlist', ['contactlist']);
+var mongojs = require ('mongojs');
+var db = mongojs('contactlist',['contactlist']);
 
 /* GET home page. */
-router.get('/', function (req, res) {
-  var myArtwork = [];
-  var myProducts = [];
-  myProducts = appdata.products;
-  appdata.products.forEach(function (item) {
-    myArtwork = myArtwork.concat(item.artwork);
-  });
+router.get('/', function(req, res) {
+    var myArtwork = [];
+    var myProducts = [];
+    myProducts = appdata.products;
+    appdata.products.forEach(function(item) {
+        myArtwork = myArtwork.concat(item.artwork);
+            });
   res.render('index', {
-    title: 'Home',
-    artwork: myArtwork,
-    products: myProducts,
-    page: 'home'
-  });
+      title: 'Home',
+      artwork: myArtwork,
+      products:myProducts,
+      page:'home'
+    });
 });
 
 /* Get Products page */
-router.get('/products', function (req, res) {
-  var myArtwork = [];
-  var myProducts = [];
+router.get('/products', function(req, res) {
+    var myArtwork = [];
+    var myProducts = [];
 
-  myProducts = appdata.products;
-  appdata.products.forEach(function (item) {
-    myArtwork = myArtwork.concat(item.artwork);
-  });
+    myProducts = appdata.products;
+    appdata.products.forEach(function(item) {
+        myArtwork = myArtwork.concat(item.artwork);
+            });
   res.render('products', {
-    title: 'Products',
-    artwork: myArtwork,
-    products: myProducts,
-    page: 'productList'
-  });
+      title: 'Products',
+      artwork: myArtwork,
+      products: myProducts,
+      page:'productList'
+    });
 });
 
 /* Get Product detail page */
-router.get('/products/:productid', function (req, res) {
-  var myArtwork = [];
-  var myProducts = [];
-  appdata.products.forEach(function (item) {
-    if (item.shortname == req.params.productid) {
-      myProducts.push(item);
-      myArtwork = myArtwork.concat(item.artwork);
-    }
-  });
+router.get('/products/:productid', function(req, res) {
+    var myArtwork = [];
+    var myProducts = [];
+    appdata.products.forEach(function(item) {
+        if(item.shortname == req.params.productid){
+            myProducts.push(item);
+        myArtwork = myArtwork.concat(item.artwork);
+        }
+            });
   res.render('products', {
-    title: 'Products',
-    artwork: myArtwork,
-    products: myProducts,
-    page: 'productDetail'
-  });
+      title: 'Products',
+      artwork: myArtwork,
+      products:myProducts,
+      page:'productDetail'
+    });
 });
 
-router.get('/contactlist', function (req, res) {
-  console.log("I received a GET request");
+router.get('/contactlist', function(req,res){
+console.log("I received a GET request");
 
-  return db.contactlist.find(function (err, docs) {
-    console.log(docs);
+return db.contactlist.find(function (err, docs) {
     return res.render('admin', {
       title: 'Admin',
       contactlist: docs
     });
   });
+});
+
+router.post('/contactlist' , function(req,res){
+  console.log(req.body);
+  db.contactlist.insert(req.body, function( err,doc){
+    res.json(doc);
+  });
+});
+
+router.delete('/contactlist/:id' , function (req,res){
+  var id = req.params.id;
+  console.log(id);
 });
 
 module.exports = router;

c5ac5abd5a902472ea4c8585c18aaa21e16909ce
Razvan Laurus <razvan.laurus@gmail.com>
2015-05-06 21:13:31 +0200

    Update functionality for contacts CRUD

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -6,39 +6,65 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
 
   console.log('Hello world from controller');
 
-  var refresh = function () {
+  $scope.contact = {};
+  $scope.isEditMode = false;
+
+  $scope.refresh = function () {
     $http.get('/api/contact')
       .success(function (res) {
         $scope.contactlist = res;
       });
   };
 
-  refresh();
-
   $scope.addContact = function () {
     console.log($scope.contact);
     $http.post('/api/contact', $scope.contact)
       .success(function (res) {
         $scope.contactlist.push(res);
+        $scope.contact = {};
       });
   };
 
   $scope.removeContact = function (id) {
     $http.delete('/api/contact/' + id)
       .success(function () {
-        for (var i = $scope.contactlist.length - 1; i >= 0; i--) {
-          if ($scope.contactlist[i]._id === id)
-            $scope.contactlist.splice(i, 1);
-        }
+        $scope.contactlist.splice(getDocIdx(id, $scope.contactlist), 1);
       });
   };
 
-  $scope.editContact = function(id) {
-
-   $http.get('api/contact/' + id)
-        .success(function(res){
-            $scope.contact = res;
-   });
+  $scope.editContact = function (id) {
+    $scope.contact = angular.copy(getDoc(id, $scope.contactlist));
+    $scope.isEditMode = true;
   };
 
+  $scope.updateContact = function () {
+    $http.put('/api/contact/' + $scope.contact._id, $scope.contact)
+      .success(function (res) {
+        $scope.contactlist[getDocIdx($scope.contact._id, $scope.contactlist)] =
+          res;
+        $scope.contact = {};
+        $scope.isEditMode = false;
+      });
+  };
+
+  $scope.cancelEdit = function () {
+    $scope.contact = {};
+    $scope.isEditMode = false;
+  };
+
+  $scope.refresh();
+
+  function getDoc(id, collection) {
+    for (var i = collection.length - 1; i >= 0; i--) {
+      if (collection[i]._id === id)
+        return collection[i];
+    }
+  }
+
+  function getDocIdx(id, collection) {
+    for (var i = collection.length - 1; i >= 0; i--) {
+      if (collection[i]._id === id)
+        return i;
+    }
+  }
 }]);
--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,7 @@ router.get('/products/:productid', function (req, res) {
   var myArtwork = [];
   var myProducts = [];
   appdata.products.forEach(function (item) {
-    if (item.shortname == req.params.productid) {
+    if (item.shortname === req.params.productid) {
       myProducts.push(item);
       myArtwork = myArtwork.concat(item.artwork);
     }
@@ -69,23 +69,55 @@ router.get('/contactlist', function (req, res) {
  ******************************************/
 
 router.get('/api/contact', function (req, res) {
-  return db.contactlist.find(function (err, contacts) {
-    return res.jsonp(contacts);
-  });
-});
+    return db.contactlist.find(function (err, contacts) {
+      if (err)
+        return res.status(500)
+          .jsonp(err);
 
-router.post('/api/contact', function (req, res) {
-  return db.contactlist.insert(req.body, function (err, doc) {
-    res.jsonp(doc);
+      return res.jsonp(contacts);
+    });
+  })
+  .post('/api/contact', function (req, res) {
+    return db.contactlist.insert(req.body, function (err, contact) {
+      if (err)
+        return res.status(500)
+          .jsonp(err);
+
+      res.jsonp(contact);
+    });
   });
-});
 
 router.get('/api/contact/:id', function (req, res) {
     return db.contactlist.find({
       _id: mongojs.ObjectId(req.params.id)
-    }, function (err, contacts) {
-      return res.jsonp(contacts);
+    }, function (err, contact) {
+      if (err)
+        return res.status(500)
+          .jsonp(err);
+
+      return res.jsonp(contact);
     });
+  })
+  .put('/api/contact/:id', function (req, res) {
+    // make sure we don't interfere with the _id
+    delete req.body._id;
+
+    return db.contactlist.update({
+      _id: mongojs.ObjectId(req.params.id)
+    }, req.body, function (err, updated) {
+      if (err)
+        return res.status(500)
+          .jsonp(err);
+      else if (updated.ok) {
+        // we get it from req.body to avoid one more query
+        req.body._id = req.params.id;
+        return res.jsonp(req.body);
+      } else {
+        return res.status(500)
+          .jsonp(new Error('Not updated'));
+      }
+    });
+
   })
   .delete('/api/contact/:id', function (req, res) {
     return db.contactlist.remove({
@@ -96,19 +128,11 @@ router.get('/api/contact/:id', function (req, res) {
           .jsonp(err);
       } else {
         return res.status(200)
-          .end();
+          .jsonp({
+            message: 'success'
+          });
       }
     });
   });
-
-router.get('/api/contact/:id' , function (req, res){
-  var id = req.params.id;
-  console.log(id);
-   db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err,doc){
-    res.json(doc);
-   });
-
-
-});
 
 module.exports = router;
